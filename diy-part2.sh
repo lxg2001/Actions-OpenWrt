b5352be9db1e1c618193d0a25a5828a67fb1a02a
lxg2001 <41837661+lxg2001@users.noreply.github.com>
2023-06-16 05:19:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$.rT.cU4J$wyLRZI4h2AaJMCQBZVYX90:19448:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 #更换默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 
 sed -i '/mediaurlbase/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i '/mediaurlbase/d' feeds/luci/themes/luci-theme-material/root/etc/uci-defaults/30_luci-theme-material
@@ -34,11 +34,15 @@ sed -i '/mediaurlbase/d' feeds/small8/luci-theme-tomato/root/etc/uci-defaults/30
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon-mod
 rm -rf feeds/luci/themes/luci-theme-design
-rm -rf feeds/small8/luci-theme-argone 
-rm -rf feeds/small8/luci-app-argone-config
+rm -rf feeds/small8/luci-theme-argon 
+rm -rf feeds/small8/luci-app-argon-config
 
-mv feeds/small8/luci-theme-argon feeds/luci/themes
 mv feeds/small8/luci-theme-design feeds/luci/themes
+
+#添加sms-tool中文支持
+sed -i '/firstchild/d' feeds/small8/luci-app-sms-tool/luasrc/controller/modem/sms.lua
+sed -i 's/luci.controller.modem.sms/luci.controller.sms/g' feeds/small8/luci-app-sms-tool/luasrc/controller/modem/sms.lua
+sed -i 's/modem/services/g' feeds/small8/luci-app-sms-tool/luasrc/controller/modem/sms.lua
 
 #TTYD自动登录
 sed -i 's/login/login -f root/g' feeds/packages/utils/ttyd/files/ttyd.config
@@ -63,13 +67,13 @@ echo "uci commit system" >> package/lean/default-settings/files/zzz-default-sett
 echo "" >> package/lean/default-settings/files/zzz-default-settings
 
 #wifi
+sed -i 's/set wireless.radio${devidx}/set wireless.radio0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/set wireless.default_radio${devidx}/set wireless.default_radio0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 echo "uci set wireless.radio0.channel=auto" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.radio0.band=5g" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.radio0.htmode=HE80" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.radio0.disabled=0" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.radio0.country=US" >> package/lean/default-settings/files/zzz-default-settings
-echo "uci set wireless.radio0.legacy_rates=1" >> package/lean/default-settings/files/zzz-default-settings
-echo "uci set wireless.radio0.mu_beamformer=0" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.default_radio0=wifi-iface" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.default_radio0.device=radio0" >> package/lean/default-settings/files/zzz-default-settings
 echo "uci set wireless.default_radio0.network=lan" >> package/lean/default-settings/files/zzz-default-settings
